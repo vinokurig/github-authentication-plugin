cb5ae057c867142f2a3dcbe4019a15d18cb0d2bb
Igor Vinokur <ivinokur@redhat.com>
2020-11-06 12:22:35 +0200

    Handle multiple sssions

--- src/github-authentication-plugin.ts
+++ src/github-authentication-plugin.ts
@@ -4,37 +4,43 @@
  */
 
 import * as theia from '@theia/plugin';
+import { v4 } from 'uuid';
 
-export function start(context: theia.PluginContext) {
-    let session: theia.AuthenticationSession | undefined;
+export function start(context: theia.PluginContext){
+    const sessions: theia.AuthenticationSession[] = context.workspaceState.get('sessions') || [];
     const onDidChangeSessions = new theia.EventEmitter<theia.AuthenticationProviderAuthenticationSessionsChangeEvent>();
     theia.authentication.registerAuthenticationProvider({
-        id: 'github',
-        label: 'GitHub',
-        supportsMultipleAccounts: false,
-        onDidChangeSessions: onDidChangeSessions.event,
-        getSessions: async () => {
-            if (session) {
-                return [session];
-            } else {
-                return [];
+            id: 'github',
+            label: 'GitHub',
+            supportsMultipleAccounts: false,
+            onDidChangeSessions: onDidChangeSessions.event,
+            getSessions: async () => sessions,
+            login: async (scopeList: string[]) => {
+                const session = {
+                    id: v4(),
+                    accessToken: 'paste the github token here',
+                    account: { label: 'githubUser', id: 'githubUserId' },
+                    scopes: scopeList
+                };
+                const sessionIndex = sessions.findIndex(s => s.id === session.id);
+                if (sessionIndex > -1) {
+                    sessions.splice(sessionIndex, 1, session);
+                } else {
+                    sessions.push(session);
+                }
+                context.workspaceState.update('sessions', sessions);
+                onDidChangeSessions.fire({ added: [session.id], removed: [], changed: [] });
+                return session;
+            },
+            logout: async (id: string) => {
+                const sessionIndex = sessions.findIndex(session => session.id === id);
+                if (sessionIndex > -1) {
+                    sessions.splice(sessionIndex, 1);
+                    context.workspaceState.update('sessions', sessions);
+                    onDidChangeSessions.fire({ added: [], removed: [id], changed: [] });
+                }
             }
-        },
-        login: async (scopeList: string[]) => {
-            session = {
-                id: 'github-session',
-                accessToken: 'paste the github token here',
-                account: { label: 'githubUser', id: 'githubUserId' },
-                scopes: scopeList
-            };
-            onDidChangeSessions.fire({ added: [session.id], removed: [], changed: [] });
-            return session;
-        },
-        logout: async (id: string) => {
-            session = undefined;
-            onDidChangeSessions.fire({ added: [], removed: [id], changed: [] });
         }
-    }
     );
 
 }
